7065131aed52db3e688379bec6d2690c1534de01
db-hasan <infoalihasanbd@gmail.com>
2023-12-24 23:40:31 +0600

    updy

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import './assets/main.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
-import './assets/backend/js/jquery-3.7.1.min.js'
-import './assets/backend/js/dataTables.js'
-import './assets/backend/js/chart.js'
-import './assets/backend/js/popper.min.js'
+import './assets/js/jquery-3.7.1.min.js'
+import './assets/js/dataTables.js'
+import './assets/js/chart.js'
+import './assets/js/popper.min.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
